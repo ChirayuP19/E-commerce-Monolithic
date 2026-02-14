06eb709dc677ccbe64bdfc2828f344145aa46258
ChirayuP19 <chirayu010675@gmail.com>
2026-02-14 00:34:24 -0500

    Entity and RestController created

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce;
+package com.ecommerce.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ public class SbEcomApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbEcomApplication.class, args);
-        System.out.println("Running fine ");
+
 	}
 
 }
