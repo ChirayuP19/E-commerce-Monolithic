2b45a23f65f34e72da570cac7e829cfcaaf45323
ChirayuP19 <chirayu010675@gmail.com>
2026-02-14 23:09:39 -0500

    Postgres add from Docker

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,12 +1,17 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Category {
 
+    @Id
     private Long categoryId;
     private String categoryName;
 }
